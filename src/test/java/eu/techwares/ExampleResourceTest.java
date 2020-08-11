3d292fea50b83770482aeb6c2b91b3a130a632c7
Gints Polis <gints.polis@balcia.com>
2020-08-11 18:11:36 +0300

    Tests using H2 DB

--- src/test/java/eu/techwares/ExampleResourceTest.java
+++ src/test/java/eu/techwares/ExampleResourceTest.java
@@ -1,12 +1,15 @@
 package eu.techwares;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class ExampleResourceTest {
 
     @Test
