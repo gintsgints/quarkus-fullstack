5fecf155034769998ebcfef24a5f7ed5d633d044
Gints Polis <gints.polis@balcia.com>
2020-08-13 21:17:50 +0300

    Top menu

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Navbar />
+    <div id="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -17,16 +27,13 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+#container {
+  padding:20px;
+  margin-top:30px;
+  height:1500px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin:0;
 }
 </style>
---
+++ webapp/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li><router-link class="active" to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+a.router-link-exact-active {
+  background-color: #4CAF50;
+}
+</style>
