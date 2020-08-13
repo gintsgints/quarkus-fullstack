5f349e73e87f9b71cd1821bb49d1c1f303830e49
Gints Polis <gints.polis@balcia.com>
2020-08-13 23:12:15 +0300

    Nicer button

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -37,6 +37,35 @@ body {
   margin:0;
 }
 
+button {
+  display: inline-block;
+  padding: 0.7em 1.7em;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.2em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  font-weight:400;
+  color:#FFFFFF;
+  background-color:#4CAF50;
+  box-shadow:inset 0 -0.6em 1em -0.35em rgba(0,0,0,0.17),inset 0 0.6em 2em -0.3em rgba(255,255,255,0.15),inset 0 0 0em 0.05em rgba(255,255,255,0.12);
+  text-align:center;
+  position:relative;
+  border: 0;
+  margin: 8px 4px;
+}
+
+button:active {
+  box-shadow:inset 0 0.6em 2em -0.3em rgba(0,0,0,0.15),inset 0 0 0em 0.05em rgba(255,255,255,0.12);
+}
+
+@media all and (max-width:30em){
+ button{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+
 .lds-hourglass {
   display: inline-block;
   position: relative;
