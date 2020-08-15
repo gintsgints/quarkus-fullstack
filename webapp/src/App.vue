177427de637975ed56b2c0d8f037616004148b30
Gints Polis <gints.polis@balcia.com>
2020-08-15 11:04:20 +0300

    Buttons have same font family.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -54,8 +54,6 @@ button {
   border-radius: 0.2em;
   box-sizing: border-box;
   text-decoration:none;
-  font-family:'Roboto',sans-serif;
-  font-weight:400;
   color:#FFFFFF;
   background-color:#4CAF50;
   box-shadow:inset 0 -0.6em 1em -0.35em rgba(0,0,0,0.17),inset 0 0.6em 2em -0.3em rgba(255,255,255,0.15),inset 0 0 0em 0.05em rgba(255,255,255,0.12);
